55f2fb3823d6a7f61983d5adb16f94c1bca47c43
GuilhermeCamposPPCAR <guilherme.campos@ppcar.com.br>
2020-04-22 23:50:41 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Departaments> list = new List<Departaments>();
+            list.Add(new Departaments { Id = 1, Name = "Eletronicos" });
+            list.Add(new Departaments { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
